2e8693c2fd54e358f9028cdfc2efacfc1e55b28a
Muhammad Ardan Hilal <muhammadardanhilal@MacBook-Air-Muhammad-2.local>
2026-03-10 17:46:18 +0700

    update booking handler

--- internal/domain/services/booking_service.go
+++ internal/domain/services/booking_service.go
@@ -1,4 +1,12 @@
 package services
 
+import (
+	"context"
+
+	"github.com/Ardnh/be-coworking-space-booking-app/internal/application/dto"
+)
+
 type BookingService interface {
+	CreateBooking(c context.Context, booking *dto.CreateBookingDto) (*dto.BookingDto, error)
+	CancelBooking(c context.Context, bookingId string) error
 }
--- internal/interfaces/http/handlers/booking_handler.go
+++ internal/interfaces/http/handlers/booking_handler.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"github.com/Ardnh/be-coworking-space-booking-app/internal/domain/services"
+	http "github.com/Ardnh/be-coworking-space-booking-app/internal/interfaces/http/responses"
 	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
 
@@ -18,4 +20,19 @@ func NewBookingHandler(service services.BookingService, validator *validator.Val
 		validator: validator,
 		log:       log,
 	}
+}
+
+func (h *BookingHandler) CreateBooking(c *fiber.Ctx) error {
+
+	return http.NewSuccessResponse(c, fiber.StatusOK, "OK", nil)
+}
+
+func (h *BookingHandler) CancelBooking(c *fiber.Ctx) error {
+
+	bookingId := c.Params("bookingId", "")
+	if bookingId == "" {
+		return http.NewErrorResponse(c, fiber.StatusBadRequest, "bookingId is required", nil)
+	}
+
+	return http.NewSuccessResponse(c, fiber.StatusOK, "Cancel OK", nil)
 }
